c91c16b356afd21025648cd75d3d9419d02633cd
Wyatt DeRamus <deramus.wyatt@gmail.com>
2023-01-02 12:20:42 -0600

    HelloWorld exercise completed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,35 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.print("Hello, World! ");
+        System.out.println("WE SEE YOU!");
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+        String myString = "3.14159";
+        System.out.println(myString);
+        double myNumber = 3.14;
+        myNumber = (long) 3.14;
+//        float myNumber = 123;
+        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        String class = ("45");
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(y);
+        System.out.println(x);
     }
 }
