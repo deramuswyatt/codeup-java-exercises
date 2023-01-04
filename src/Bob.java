f152bb15623afeebd28da3331143807c267a90d3
Wyatt DeRamus <deramus.wyatt@gmail.com>
2023-01-04 15:45:29 -0600

    completed Bob exercise

---
+++ src/Bob.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("My name is Bob, let's have a chat.");
+        String userInput = keyboard.nextLine();
+        if (userInput.endsWith("?")){
+            System.out.println("Sure");
+        } else if (userInput.endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        } else if (userInput.equals("")) {
+            System.out.println("Fine. Be that way!");
+        } else if (userInput.contains("t")) {
+            System.out.println("you got it Wyatt.");
+        } else {
+            System.out.println("whatever");
+        }
+    }
+}
